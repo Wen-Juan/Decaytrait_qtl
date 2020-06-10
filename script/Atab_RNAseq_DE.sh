a22f9ace4d92fabc4cf0581d64fd5203f3d03452
Wen-Juan Ma <wenjuanma84@gmail.com>
2020-06-10 12:23:41 -0400

    Update Atab_RNAseq_DE.sh

--- script/Atab_RNAseq_DE.sh
+++ script/Atab_RNAseq_DE.sh
@@ -1,1 +1,21 @@
+      ####Step1. trim the transcriptome a bit#####
 ###trim the trinity generated de novo transcriptome.
+perl remove_short_transcript.sql 300 TCAGTT_Trinity.fasta > Atab_trinity_300bp.fa
+
+###check the gene and transcript number using Trinity perl script
+/usr/local/bio/trinityrnaseq/util/TrinityStats.pl Atab_trinity_300bp.fa
+
+###################################
+## Counts of transcripts, etc.
+################################
+Total trinity 'genes':	21965
+Total trinity transcripts:	26751
+Percent GC: 44.58
+###################################
+
+
+      ####Step2. Quantify accounts of transcripts#####
+####2.1 index the trascriptome
+/usr/local/bio/kallisto/kallisto index -i Atab_trinity.idx Atab_trinity_300bp.fa
+
+####2.2 run kallisto to quantify the expression
