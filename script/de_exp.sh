5f3efcfaa38642a963a0fbb2fb7a67e7ee820147
Wen-Juan Ma <wenjuanma84@gmail.com>
2020-06-11 14:00:04 -0400

    add .

--- script/de_exp.sh
+++ script/de_exp.sh
@@ -1,9 +1,6 @@
-/usr/local/bio/kallisto/kallisto
-/usr/local/bio/trinityrnaseq/util/R/
-
-/usr/local/bio/trinityrnaseq/Analysis/DifferentialExpression/run_DE_analysis.pl \
---matrix Atab_.isoform.counts.matrix \
+perl run_DE_analysis.pl \
+--matrix ../input/Atab_.isoform.counts.matrix \
 --method edgeR \
 --min_reps_min_cpm 3 \
---output DE_exp \
+--output ../output/DE_exp \
 --dispersion 0.1
