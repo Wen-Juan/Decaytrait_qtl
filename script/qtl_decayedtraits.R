1d1b1faee199a5f9daf0c34287f440f8818eb7c5
Wen-Juan Ma <wenjuanma84@gmail.com>
2020-06-24 17:12:12 -0400

    add .

--- script/qtl_decayedtraits.R
+++ script/qtl_decayedtraits.R
@@ -167,19 +167,114 @@ lod
 lod_threshold <- summary(operm.hk, alpha = 0.05) 
 labels_df <- as.data.frame(summary(out.hk, perms = operm.hk, alpha = 0.05, pvalues = TRUE))
 
-summary(out.hk, perms=operm.hk, alpha=0.05, pvalues=TRUE)
+#################
+#################
+#################
+summary(out.hk, perms=operm.hk, lodcolumn = 1, alpha=0.05, pvalues=TRUE) ### alpha=0.05 5% is above this threshold. 95% are below. 
+##Permutation = 1000
+chr pos  lod pval
+c12.loc10  12  10 40.9    0
+##
+#################
+#################
+#################
+
+summary(out.hk, perms=operm.hk, lodcolumn = 1,alpha=0.10, pvalues=TRUE)
 ##Permutation = 1000
 chr pos  lod pval
 c12.loc10  12  10 40.9    0
 ##
 
-summary(out.em, perms=operm.em, alpha=0.05, pvalues=TRUE)
+summary(out.hk, perms=operm.hk, lodcolumn = 1,alpha=0.60, pvalues=TRUE)
+##Permutation = 1000
+chr pos   lod  pval
+c5.loc47    5  47  1.56 0.364
+c9.loc45    9  45  1.53 0.367
+c12.loc10  12  10 40.89 0.000
+##
+
+summary(out.em, perms=operm.em, lodcolumn = 1,alpha=0.05, pvalues=TRUE)
 ##Permutation = 1000
 chr pos  lod pval
 c12.loc8  12   8 41.3    0
 
+summary(out.em, perms=operm.em, lodcolumn = 1,alpha=0.10, pvalues=TRUE)
+##Permutation = 1000
+chr pos  lod pval
+c12.loc8  12   8 41.3    0
+
+summary(out.em, perms=operm.em, lodcolumn = 1,alpha=0.60, pvalues=TRUE)
+##Permutation = 1000
+chr pos   lod  pval
+c5.loc47   5  47  1.55 0.331
+c9.loc45   9  45  1.53 0.344
+c12.loc8  12   8 41.30 0.000
+
+mname1 <- find.marker(qtl, chr = 12, pos = 10)
+mname1 
+#[1] "Ajap26"
+
+mname2 <- find.marker(qtl, chr = 12, pos = 8)
+mname2 
+#[1] "Ajap26"
+
+mname3 <- find.marker(qtl, chr = 12, pos = 2)
+mname3 
+#[1] "Ajap26"
+
+mname4 <- find.marker(qtl, chr = 12, pos = 23)
+mname4 
+#[1] "Ajap26"
+
+effectplot(qtl, pheno.col = 1, mname1=mname1)
+###heterozygosity is responsible for mating success.
+
+effectplot(qtl, pheno.col = 1, mname1=mname4)
+###heterozygosity is responsible for mating success.
 
 
+pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Decaytrait_qtl/output/qtl_lod_2qtl_effectplotv2.pdf",  width=12, height=6)
+par(mfrow=c(1,2)) 
+par(mar=c(5,5,4,3))
+effectplot(qtl, pheno.col = 1, mname1=mname1, draw=TRUE, xlab="Linkage group 12", ylab ="Proportion mating success",ylim = c(0,1))
+effectplot(qtl, pheno.col = 1, mname1=mname4, draw=TRUE,xlab="Linkage group 12", ylab ="Proportion mating success",ylim = c(0,1))
+dev.off()
+
+
+CIchr12v1 <- bayesint(out.hk, chr = 12, prob = 0.95)
+####
+chr pos      lod
+c12.loc7   12   7 40.02445
+c12.loc10  12  10 40.88967
+c12.loc13  12  13 40.03487
+#####
+plot(out.hk, chr=12, lodcolumn=1, main="Confidence interval for QTL at LG12", xlab="Linkage map", ylab="LOD", col = "blue", lwd =2)
+lines(x=CIchr12v1[c(1,3),2], y=c(0,0), type="l", col="blue", lwd=4)
+
+CIchr12v2 <- bayesint(out.em, chr = 12, prob = 0.95)
+###
+chr pos      lod
+c12.loc5   12   5 40.59870
+c12.loc8   12   8 41.29667
+c12.loc12  12  12 40.70013
+###
+plot(out.em, chr=12, lodcolumn=1, main="Confidence interval for QTL at LG12", xlab="Linkage map", ylab="LOD", col = "dark green", lwd =2)
+lines(x=CIchr12v2[c(1,3),2], y=c(0,0), type="l", col="dark green", lwd=4)
+
+
+pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Decaytrait_qtl/output/qtl_lod_genomewide_all2.pdf", width=12, height=8)
+plot(out.hk, chr=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16),col="dark green",ylim=c(0,40),xlab="Linkage group", ylab="LOD",lwd = 2,alternate.chrid=TRUE)
+plot(out.em, chr=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16), col="blue", add=TRUE,ylim=c(0,42),xlab="Linkage group", ylab="LOD",lwd = 2,alternate.chrid=TRUE)
+abline(h = 2.47,col="blue", lwd=2, lty=2)
+dev.off()
+
+pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Decaytrait_qtl/output/qtl_lod_genomewide_lg12_confidenceinterval.pdf", width=10, height=8)
+plot(out.em, chr=12, lodcolumn=1, main="Confidence interval for QTL at LG12", xlab="Linkage group 12", ylab="LOD", col = "dark green", lwd =2,lternate.chrid=TRUE,cex.lab=1.5)
+plot(out.hk, chr=12, lodcolumn=1, xlab="Linkage map", ylab="LOD", col = "blue", lwd =2,add=TRUE,ylim=c(0,42),lternate.chrid=TRUE,cex.lab=1.5)
+lines(x=CIchr12v2[c(1,3),2], y=c(0,0), type="l", col="dark green", lwd=4)
+lines(x=CIchr12v1[c(1,3),2], y=c(0,0), type="l", col="blue", lwd=4)
+abline(h = 2.47,col="black", lwd=1.5, lty=2)
+dev.off()
 
 ###########
 ###########
@@ -304,14 +399,94 @@ qtl_plot(input = rbind(data.frame(out.em, method = "EM algorithm"),
          labels = labels_df)
 dev.off()
 
-pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Decaytrait_qtl/output/qtl_lod_genomewide_permutation1000_2modles.pdf", width=10, height=8)
+pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Decaytrait_qtl/output/qtl_lod_lg12_permutation1000_2modles.pdf", width=10, height=8)
 qtl_plot(input = rbind(data.frame(out.em, method = "EM algorithm"), 
                        data.frame(out.hk, method = "Haley-Knott regression")), 
          lod = lod_threshold[1], 
          chrs = c(12),
          rug = TRUE, 
-         labels = labels_df)
+         labels = labels_df) 
 dev.off()
+
+###fir the model for single QTL
+#operm.em
+multisim <- sim.geno(qtl, step=1, n.draws=10000, err=0.001)
+multisim2 <- calc.genoprob(multisim, step=1, error.prob=0.001)
+
+m1lg <- 12
+m1cm <- 10
+#m2lg <- 12
+#m2cm <- 23
+
+Cross <- data.frame(Cross=as.numeric(pull.pheno(qtl, "cross") == "BC"))
+f1 <- "y ~ Cross + Q1 + Cross:Q1"
+
+# Input number of loci for the qtl object
+qtl.auto <- makeqtl(multisim2, chr=c(m1lg), pos=c(m1cm), what="draws")
+fq.auto <- fitqtl(multisim2, cov=Cross,qtl=qtl.auto, formula=f1,model='binary')
+summary(fq.auto, alpha=0.05, pvalues=TRUE, format="allpheno")
+
+###
+fitqtl summary
+
+Method: multiple imputation 
+Model:  binary phenotype
+Number of observations : 253 
+
+Full model result
+----------------------------------  
+  Model formula: y ~ Cross + Q1 + Cross:Q1 
+
+df      LOD     %var Pvalue(Chi2)
+Model  3 35.90548 47.98097            0
+
+
+Drop one QTL at a time ANOVA table: 
+  ----------------------------------  
+  df   LOD  %var Pvalue(Chi2)    
+Cross          2  0.00  0.00            1    
+12@10.0        2 35.91 47.98       <2e-16 ***
+  Cross:12@10.0  1  0.00  0.00            1    
+---
+  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+###
+# check for interactions between qtl or with qtl and cross
+qtl.auto.ai <- addint(multisim2, qtl=qtl.auto, formula=f1, pheno.col=1, covar=Cross)
+summary(qtl.auto.ai, alpha=0.05, pvalues=TRUE, format="allpheno")
+
+# refine positions
+rqtl <- refineqtl(multisim2, qtl=qtl.auto, formula=f1, pheno.col=1, method=c("imp"), covar=Cross,model='binary')
+final.fit <- fitqtl(multisim2, qtl=rqtl, formula=f1, pheno.col=1, covar=Cross, model='binary')
+summary(final.fit, alpha=0.05, pvalues=TRUE, format="allpheno")
+###
+fitqtl summary
+
+Method: multiple imputation 
+Model:  binary phenotype
+Number of observations : 253 
+
+Full model result
+----------------------------------  
+  Model formula: y ~ Cross + Q1 + Cross:Q1 
+
+df      LOD     %var Pvalue(Chi2)
+Model  3 36.64514 48.67664            0
+
+
+Drop one QTL at a time ANOVA table: 
+  ----------------------------------  
+  df   LOD  %var Pvalue(Chi2)    
+Cross         2  0.00  0.00            1    
+12@6.0        2 36.65 48.68       <2e-16 ***
+  Cross:12@6.0  1  0.00  0.00            1    
+---
+  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+###
+
+capture.output(summary(final.fit, alpha=0.05, pvalues=TRUE, format="allpheno"), file=file.path(outpath, paste("pheno",'2qtl_PVE_table.txt', sep="_")))
+
+M1
+M2
 
 ########################################
 ########################################two-QTL genome scan
@@ -319,13 +494,12 @@ dev.off()
 
 # The function scantwo performs a two-dimensional genome scan with a two-QTL model. For every pair of positions, it calculates a LOD score for the full model (two QTL plus interaction) and a LOD score for the additive model (two QTL but no interaction). This be quite time consuming, and so you may wish to do the calculations on a coarser grid.
 qtl <- calc.genoprob(qtl, step=1, error.prob=0.001)
-Cross <- data.frame(Cross=as.numeric(pull.pheno(qtl, "cross") == "F2"))
+Cross <- data.frame(Cross=as.numeric(pull.pheno(qtl, "cross") == "BC"))
 ###hk model
 out2.hk <- scantwo(qtl, pheno.col=1, addcovar=Cross, model=c("binary"),method="hk")
 summary(out2.hk)
-max(out2.hk)
 
-plot(out2.hk)
+plot(out2.hk) #here is the heatmap of LOD scores.
 pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Decaytrait_qtl/output/qtl_hk_scantwo_lod.pdf", width=12, height=12)
 plot(out2.hk, chr=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16))
 dev.off()
@@ -334,14 +508,11 @@ max(out2.hk)
 ###02062020
 ###
 pos1f pos2f lod.full lod.fv1 lod.int     pos1a pos2a lod.add lod.av1
-c2:c12    43     9     40.2    1.14   0.446        44     9    39.7   0.693
+c2:c12    43    10     40.2    1.16   0.465        44    10    39.7   0.696
 ###
-19062020
-#       pos1f pos2f lod.full lod.fv1 lod.int     pos1a pos2a lod.add lod.av1
-c3:c12    34    11     41.8   0.892   0.286        35    10    41.5   0.606
 
 #One may also use scantwo to perform permutation tests in order to obtain genome-wide LOD significance thresholds.
-operm2.hk <- scantwo(qtl, method="hk", n.perm=10, model='binary')
+operm2.hk <- scantwo(qtl, pheno.col=1, addcovar=Cross, method="hk", n.perm=5, model='binary')
 
 summary(operm2.hk,alpha=0.05)
 ##
@@ -353,74 +524,44 @@ full  fv1  int  add  av1  one
 full  fv1  int  add  av1  one
 5% 4.95 3.15 2.92 4.69 2.21 2.51
 ##
-summary(out2.hk, perms=operm2.hk, pvalues=TRUE,
-        alphas=0.05)
+summary(out2.hk,thresholds=c(4.90, 3.83, 3.61, 3.87, 1.89),df=TRUE) ###5%
+#There were no pairs of loci meeting the criteria.
+summary(out2.hk,thresholds=c(4.57, 3.52, 3.27, 3.41, 1.69),df=TRUE) ###10%
+###
+pos1f pos2f lod.full lod.fv1 lod.int     pos1a pos2a lod.add lod.av1
+c5:c9    49    41     4.56    2.77    1.02        48    42    3.54    1.75
+###
+summary(out2.hk, perms=operm2.hk,alphas=c(0.05, 0.05, 0, 0.05, 0.05), pvalues=TRUE)
 ##
-There were no pairs of loci meeting the criteria.
+pos1f pos2f lod.full pval lod.fv1 pval lod.int pval     pos1a pos2a lod.add pval lod.av1 pval
+c5:c9    49    41     4.56  0.2    2.77    1    1.02    1        48    42    3.54    0    1.75  0.2
 ##
-plot(operm2.hk)
+plot(operm2.hk) 
+plot(out2.hk, chr=c(12),upper="cond-int")
+
+###One may also restrict the summary to just the case of j = k, to look at evidence for linked QTL on each chromosome, by using allpairs=FALSE.
+summary(out2.hk,allpairs=FALSE, df=TRUE)
+######
+pos1f pos2f lod.full lod.fv1 lod.int     pos1a pos2a lod.add lod.av1
+c1 :c1     50    55    2.721   1.546  1.3432        27    87   1.378  0.2031
+c2 :c2      0    51    1.598   0.451  0.3613         0    51   1.237  0.0894
+c3 :c3      1     2    2.657   2.412  2.2577         6     7   0.400  0.1539
+c4 :c4     51    53    0.522   0.100  0.0812        64    68   0.441  0.0191
+c5 :c5     12    65    3.180   1.387  1.1686        19    58   2.011  0.2183
+c6 :c6      0     4    1.071   0.717  0.5892         1     5   0.482  0.1276
+c7 :c7      0   256    1.822   1.597  0.9610        26    27   0.861  0.6363
+c8 :c8      0    82    1.229   0.311  0.1544         0    79   1.074  0.1569
+c9 :c9      3    68    2.945   1.285  0.8772        47    50   2.068  0.4077
+c10:c10    68    93    1.909   0.748  0.3087        17    56   1.600  0.4396
+c11:c11    10    11    2.569   1.511  1.4377         7    12   1.132  0.0734
+c12:c12     0    26   39.209   0.190  0.1521         6    28  39.057  0.0382
+c13:c13     0     0    0.588   0.000  0.0000         0     0   0.588  0.0000
+c14:c14     7    10    0.615   0.190  0.0807         1     9   0.535  0.1096
+c15:c15     0     0    0.124   0.000  0.0000         0     0   0.124  0.0000
+c16:c16     0     1    0.614   0.555  0.3490         0     1   0.265  0.2056
+####
 
 ###########
 ###########
-
 # end of rqtltour.R
-
-###pie chart
-# Load ggplot2
-library(ggplot2)
-
-# Create Data
-data <- data.frame(
-  group=LETTERS[1:2],
-  value=c(50,50)
-)
-
-# Basic piechart
-pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Decaytrait_qtl/output/piechartGO.pdf", width=12, height=12)
-ggplot(data, aes(x="", y=value, fill=group)) +
-  geom_bar(stat="identity", width=1, color="black") +
-  coord_polar("y", start=0) +
-  theme_void() +
-  scale_fill_manual(values=c("black", "white")) +
-  theme(legend.position="none") 
-dev.off()
-
-pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Decaytrait_qtl/output/piecharG1.pdf", width=12, height=12)
-ggplot(data, aes(x="", y=value, fill=group)) +
-  geom_bar(stat="identity", width=1, color="black") +
-  coord_polar("y", start=0) +
-  theme_void() +
-  scale_fill_manual(values=c("black", "white")) +
-  theme(legend.position="none") 
-dev.off()
-
-pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Decaytrait_qtl/output/piecharG2.pdf", width=12, height=12)
-ggplot(data, aes(x="", y=value, fill=group)) +
-  geom_bar(stat="identity", width=1, color="black") +
-  coord_polar("y", start=0) +
-  theme_void() +
-  scale_fill_manual(values=c("black", "white")) +
-  theme(legend.position="none") 
-dev.off()
-
-
-pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Decaytrait_qtl/output/piecharG3.pdf", width=12, height=12)
-ggplot(data, aes(x="", y=value, fill=group)) +
-  geom_bar(stat="identity", width=1, color="black") +
-  coord_polar("y", start=0) +
-  theme_void() +
-  scale_fill_manual(values=c("black", "white")) +
-  theme(legend.position="none") 
-dev.off()
-
-
-pdf("/Users/Wen-Juan/Dropbox (Amherst College)/Amherst_postdoc/github/Decaytrait_qtl/output/piecharG4.pdf", width=12, height=12)
-ggplot(data, aes(x="", y=value, fill=group)) +
-  geom_bar(stat="identity", width=1, color="black") +
-  coord_polar("y", start=0) +
-  theme_void() +
-  scale_fill_manual(values=c("black", "white")) +
-  theme(legend.position="none") 
-dev.off()
-
 
