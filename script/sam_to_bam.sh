4f1389a7d7a03b369344ac593d9b0556647873b6
Wen-Juan Ma <wenjuanma84@gmail.com>
2020-07-12 18:54:20 -0400

    add .

---
+++ script/sam_to_bam.sh
@@ -0,0 +1,1 @@
+/usr/local/bio/bwa.kit/samtools view -bS AM_AGAGAT_paired.sam > AM_AGAGAT_paired.bam
---
+++ script/sam_tobam1.sh
@@ -0,0 +1,1 @@
+/usr/local/bio/bwa.kit/samtools view -bS KG_TCAGTT_paired.sam > KG_TCAGTT_paired.bam
