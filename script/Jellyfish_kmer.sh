be5986aa0732ee29da77302eda14c84998abdd64
Wen-Juan Ma <wenjuanma84@gmail.com>
2020-07-18 12:12:33 -0400

    add .

--- script/Jellyfish_kmer.sh
+++ script/Jellyfish_kmer.sh
@@ -1,8 +1,10 @@
+####
 ####run kmer analysis from jellyfish to estimate genome size
+###
 /usr/local/bio/jellyfish/jellyfish count -m 31 -o KG_km31_fastq.counts -C -s 500000000 -t 5 KG_TCAGTT_R1_pairedR1.fastq KG_TCAGTT_R2_pairedR2.fastq
 
 ###generate histogram file
- /usr/local/bio/jellyfish/jellyfish histo KG_km31_fastq.counts > KG_km31_fastq.counts.histo
+/usr/local/bio/jellyfish/jellyfish histo KG_km31_fastq.counts > KG_km31_fastq.counts.histo
 
 ###using genomescope v2.0 was recommended to use kmer 21 for most of genomes
 ###kmer 21
--- script/sam_tobam1.sh
+++
@@ -1,1 +0,0 @@
-/usr/local/bio/bwa.kit/samtools view -bS KG_TCAGTT_paired.sam > KG_TCAGTT_paired.bam
