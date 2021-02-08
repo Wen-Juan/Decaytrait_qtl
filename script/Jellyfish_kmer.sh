284d38f2e60a7f7712fcd591bb645362afcb9cc7
Wen-Juan Ma <wenjuanma84@gmail.com>
2021-02-07 23:32:09 -0600

    Update Jellyfish_kmer.sh

--- script/Jellyfish_kmer.sh
+++ script/Jellyfish_kmer.sh
@@ -1,10 +1,10 @@
 ####
-####run kmer analysis from jellyfish to estimate genome size
+####Run kmer analysis from jellyfish to estimate genome size
 ###
-/usr/local/bio/jellyfish/jellyfish count -m 31 -o KG_km31_fastq.counts -C -s 500000000 -t 5 KG_TCAGTT_R1_pairedR1.fastq KG_TCAGTT_R2_pairedR2.fastq
+/usr/local/bio/jellyfish/jellyfish count -m 31 -o KG_km21_fastq.counts -C -s 500000000 -t 5 KG_TCAGTT_R1_pairedR1.fastq KG_TCAGTT_R2_pairedR2.fastq
 
 ###generate histogram file
-/usr/local/bio/jellyfish/jellyfish histo KG_km31_fastq.counts > KG_km31_fastq.counts.histo
+/usr/local/bio/jellyfish/jellyfish histo KG_km21_fastq.counts > KG_km21_fastq.counts.histo
 
 ###using genomescope v2.0 was recommended to use kmer 21 for most of genomes
 ###kmer 21
@@ -19,5 +19,3 @@ http://genomescope.org/genomescope2.0/analysis.php?code=93S8LzwWZNY9CJ99L05e
 ###kmer41
 http://genomescope.org/genomescope2.0/analysis.php?code=T52ri5URhNsSLSYn7csY
 
-###kmer21 AM population
-http://genomescope.org/genomescope2.0/analysis.php?code=ULemyk0pi0OnvGbcEFuv
